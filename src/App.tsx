0c84443fdc36ec8f140b21cbbfd253f757d7dea9
Elsayed Abdullah <sayedaladway@gmail.com>
2024-01-23 14:50:19 +0200

    finished filtered for todos and save todos in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TodoForm, { ITodo } from "./components/TodoForm";
 import Todo from "./components/Todo";
 
 function App() {
-  const [todos, setTodos] = useState<ITodo[]>([]);
+  const [todos, setTodos] = useState<ITodo[]>(
+    JSON.parse(localStorage.getItem("todos")!) || []
+  );
+  const [status, setStatus] = useState<string>("all");
+  const [filteredTodos, setFilteredTodos] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    filterHandler();
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos, status]);
+
+  useEffect(() => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      const localTodos = localStorage.getItem("todos");
+      setTodos(JSON.parse(localTodos!));
+    }
+  }, []);
+
+  // filter todos
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => !todo.completed));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
 
   // adding todo to the list
   function addTodo(todo: ITodo) {
@@ -16,12 +49,58 @@ function App() {
     setTodos(todos.filter((todo) => todo.id !== id));
   }
 
+  // toggle todo
+  function toggleTodo(id: string) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
+  }
+
   return (
     <div className="container">
       <TodoForm addTodo={addTodo} />
-      {todos?.map((todo) => (
-        <Todo deleteTodo={deleteTodo} {...todo} key={todo.id} />
-      ))}
+      <div className="todos-list">
+        {filteredTodos.length ? (
+          filteredTodos?.map((todo) => (
+            <Todo
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+              {...todo}
+              key={todo.id}
+            />
+          ))
+        ) : (
+          <p style={{ textAlign: "center", opacity: "0.5" }}>No todos left</p>
+        )}
+      </div>
+
+      <div>
+        <button
+          className="update-btn btn"
+          onClick={() => {
+            setStatus("all");
+          }}
+        >
+          All
+        </button>
+        <button
+          className="update-btn btn"
+          onClick={() => setStatus("uncompleted")}
+        >
+          uncompleted
+        </button>
+        <button
+          className="update-btn btn"
+          onClick={() => setStatus("completed")}
+        >
+          Completed
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,19 +1,33 @@
+import { CheckCircle, Trash } from "lucide-react";
+
 interface IProps {
   id: string;
   text: string;
   completed: boolean;
   deleteTodo: (id: string) => void;
+  toggleTodo: (id: string) => void;
 }
 
-const Todo = ({ id, text, completed, deleteTodo }: IProps) => {
+const Todo = ({ id, text, completed, deleteTodo, toggleTodo }: IProps) => {
   return (
-    <div className="d-f">
-      <p style={{ textDecoration: completed ? "line-through" : "none" }}>
+    <div className="d-f" style={{ maxWidth: "300px", margin: "10px auto" }}>
+      <p
+        style={{
+          textDecoration: completed ? "line-through" : "none",
+          opacity: completed ? 0.5 : 1,
+          flex: 1,
+        }}
+      >
         {text}
       </p>
-      <button className="btn delete-btn" onClick={() => deleteTodo(id)}>
-        &times;
-      </button>
+      <div style={{ gap: "10px", justifyContent: "flex-end", display: "flex" }}>
+        <button className="btn delete-btn" onClick={() => toggleTodo(id)}>
+          <CheckCircle color="green" />
+        </button>
+        <button className="btn delete-btn" onClick={() => deleteTodo(id)}>
+          <Trash color="red" />
+        </button>
+      </div>
     </div>
   );
 };
